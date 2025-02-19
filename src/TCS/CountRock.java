7a1d3e115455fff162c728d39b5c48c0cedcb03e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-19 23:58:18 +0530

    sgbjjgsl

---
+++ src/TCS/CountRock.java
@@ -0,0 +1,29 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class CountRock {
+
+
+
+    static void countRock(){
+
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int r=sc.nextInt();
+        int[] arr=new int[s];
+        for(int i=0; i<s; i++){
+            arr[i]=sc.nextInt();
+
+        }
+        for(int j=0; j<r; j++){
+            int min=sc.nextInt();
+            int max=sc.nextInt();
+        }
+    }
+
+}
