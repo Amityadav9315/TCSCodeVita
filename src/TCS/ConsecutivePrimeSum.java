2aa5907fb044fc89fd42e6afb1cd4159ef7d67a0
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-20 00:04:38 +0530

    fdsklngsr

--- src/TCS/ConsecutivePrimeSum.java
+++ src/TCS/ConsecutivePrimeSum.java
@@ -1,2 +1,39 @@
-package TCS;public class ConsecutivePrimeSum {
-}
+package TCS;
+
+import java.util.Scanner;
+
+public class ConsecutivePrimeSum{
+
+        static void primeSum(int n){
+
+            int count1=1;
+            int count2=0;
+            int sum=0;
+            for(int  i=2; i<n; i+
+                    }
+                    if(count1==2){
+                        sum=sum+j;
+                    }
+
+
+                }
+            }
+
+        }
+
+
+
+
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+
+
+        }
+
+
+    }
+
+
+
