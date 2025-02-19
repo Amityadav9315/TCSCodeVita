5a1b046ee62f718e23345a7e2541d7227e367117
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-20 00:31:42 +0530

    fdsklngsr

--- src/TCS/ConsecutivePrimeSum.java
+++ src/TCS/ConsecutivePrimeSum.java
@@ -6,20 +6,36 @@ public class ConsecutivePrimeSum{
 
         static void primeSum(int n){
 
+
             int count1=1;
             int count2=0;
             int sum=0;
-            for(int  i=2; i<n; i+
+            for(int i=2; i<n; i++){
+                for(int j=2; j<n; j++){
+                    if(i%j==0){
+                        count1++;
+
                     }
-                    if(count1==2){
-                        sum=sum+j;
+
                     }
+                if(count1==2){
+                    sum=sum+i;
 
 
                 }
+                for(int k=2; k<n; k++){
+                    if(sum%k==0){
+                        count2++;
+                    }
+
+                }
+
+
+                }
+            System.out.println(count2);
             }
 
-        }
+
 
 
 
@@ -28,6 +44,7 @@ public class ConsecutivePrimeSum{
         public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             int n=sc.nextInt();
+            primeSum(n);
 
 
         }
