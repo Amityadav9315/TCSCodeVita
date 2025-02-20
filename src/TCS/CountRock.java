a68a36a9407f4f18b858fde89d5bc46b44b85bea
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-20 20:50:26 +0530

    queue data structure

--- src/TCS/CountRock.java
+++ src/TCS/CountRock.java
@@ -5,25 +5,39 @@ import java.util.Scanner;
 public class CountRock {
 
 
-
-    static void countRock(){
-
-    }
-
-
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int s=sc.nextInt();
-        int r=sc.nextInt();
-        int[] arr=new int[s];
-        for(int i=0; i<s; i++){
-            arr[i]=sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int[] a = new int[1000];
+        int s, i, j, t, l1 = 0, l2 = 0, c = 0;
 
+        System.out.println("Enter the no of sample");
+        s = sc.nextInt();
+
+        System.out.println("Enter the no of range");
+        t = sc.nextInt();
+
+        System.out.println("Enter the numbers");
+        for (i = 0; i < s; i++) {
+
+            a[i] = sc.nextInt();
         }
-        for(int j=0; j<r; j++){
-            int min=sc.nextInt();
-            int max=sc.nextInt();
+
+        for (i = 0; i < t; i++) {
+            System.out.println("Enter the max and min range");
+
+            l1 = sc.nextInt();
+
+            l2 = sc.nextInt();
+            for (j = 0; j < s; j++) {
+                if ((a[j] >= l1) && (a[j] <= l2))
+
+
+                    c++;
+            }
+            System.out.println(c);
+            c = 0;
         }
+
+
     }
-
 }
