025912b539a03e16baa63c04062590821fdf4630
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-20 21:10:56 +0530

    queue data structure

--- src/TCS/CountRock.java
+++ src/TCS/CountRock.java
@@ -1,43 +1,62 @@
 package TCS;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class CountRock {
-
-
+class CountRock {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] a = new int[1000];
-        int s, i, j, t, l1 = 0, l2 = 0, c = 0;
 
-        System.out.println("Enter the no of sample");
-        s = sc.nextInt();
+        int s = sc.nextInt(); // Size of the array
+        int t = sc.nextInt(); // Number of queries
 
-        System.out.println("Enter the no of range");
-        t = sc.nextInt();
-
-        System.out.println("Enter the numbers");
-        for (i = 0; i < s; i++) {
-
+        int[] a = new int[s];
+        for (int i = 0; i < s; i++) {
             a[i] = sc.nextInt();
         }
 
-        for (i = 0; i < t; i++) {
-            System.out.println("Enter the max and min range");
+        // Sort the array for binary search
+        Arrays.sort(a);
 
-            l1 = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int l1 = sc.nextInt(); // Lower bound
+            int l2 = sc.nextInt(); // Upper bound
 
-            l2 = sc.nextInt();
-            for (j = 0; j < s; j++) {
-                if ((a[j] >= l1) && (a[j] <= l2))
+            // Find the first index >= l1
+            int leftIndex = lowerBound(a, l1);
+            // Find the first index > l2
+            int rightIndex = upperBound(a, l2);
 
-
-                    c++;
-            }
-            System.out.println(c);
-            c = 0;
+            // The count is the difference between the two indices
+            int count = rightIndex - leftIndex;
+            System.out.print(count + " ");
         }
+    }
 
+    // Binary search to find the first index >= target
+    private static int lowerBound(int[] a, int target) {
+        int left = 0, right = a.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (a[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
 
+    // Binary search to find the first index > target
+    private static int upperBound(int[] a, int target) {
+        int left = 0, right = a.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (a[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
