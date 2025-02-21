a7700e864db72af2a0e569306fbe50571468955e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-21 19:29:56 +0530

    vfxnkfv

--- src/TCS/APICalling.java
+++ src/TCS/APICalling.java
@@ -21,5 +21,16 @@ public class APICalling {
 
         System.out.println(response.statusCode());
         System.out.println(response.body());
+
+        var url1="https://api.rootnet.in/covid19-in/stats/latest";
+
+        var request1= HttpRequest.newBuilder().GET().uri(URI.create(url1)).build();
+
+
+        var client1 = HttpClient.newBuilder().build();
+        var response1=client.send(request1, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response1.statusCode());
+        System.out.println(response1.body());
     }
 }
