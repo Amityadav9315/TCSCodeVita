c1920cfa53c6615a6fd6af78a89a0377d55c7ebf
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-23 14:57:39 +0530

    vsnnfsbd

--- src/TCS/PrimeTimeAgain.java
+++ src/TCS/PrimeTimeAgain.java
@@ -13,21 +13,19 @@ public class PrimeTimeAgain {
         int count=0;
         int a=0;
         for(int i=2; i<c; i++){
-            for(int j=2; j<d; j=j+c){
-                if(i%j==0){
-                    count++;
+            for(int j=i; j<=d; j=j+c) {
+                for (int k = 2; k <= d; k++) {
+                    if (j % k == 0) {
+                        count++;
+                    }
                 }
             }
             if(count==3){
                  a++;
             }
             count=0;
-
-
         }
         System.out.println(a);
-
-
     }
 
 
@@ -35,6 +33,7 @@ public class PrimeTimeAgain {
         Scanner sc=new Scanner(System.in);
         int d=sc.nextInt();
         int p=sc.nextInt();
+        primeTimeagain(d,p);
 
 
     }
