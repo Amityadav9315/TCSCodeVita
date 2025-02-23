b2a88b0591415b1df3888e3ee1ce02a0011d4330
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-23 14:49:39 +0530

    vsnnfsbd

--- src/TCS/PrimeTimeAgain.java
+++ src/TCS/PrimeTimeAgain.java
@@ -10,13 +10,22 @@ public class PrimeTimeAgain {
     static  void primeTimeagain(int d,int p){
 
         int c=d/p;
+        int count=0;
+        int a=0;
         for(int i=2; i<c; i++){
-            for(int j=0; j<p; j++){
-                if(i=isprime&&i+c=isprime&&)
+            for(int j=2; j<d; j=j+c){
+                if(i%j==0){
+                    count++;
+                }
             }
+            if(count==3){
+                 a++;
+            }
+            count=0;
 
 
         }
+        System.out.println(a);
 
 
     }
