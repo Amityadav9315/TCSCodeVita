2e22ee9a09f60dc6217aadf79ef77bcf06da8cc0
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-25 08:58:05 +0530

    dwnklqef

--- src/TCS/MinSum.java
+++ src/TCS/MinSum.java
@@ -5,15 +5,36 @@ import java.util.Scanner;
 public class MinSum {
 
 
-    static void minSum(int n,int r,int[] arr){
+    static void minSum(int n,int k,int[] arr){
+        int[] array2=new int[n];
+
+
         int max=0;
 
         for(int i=0; i<n; i++){
             if(arr[i]>max){
                 max=arr[i];
             }
+            for(int j=0; j<k;  j++){
+                max=max/2;
+                arr[i]=max;
+            }
 
 
+        }
+
+        for(int i=k; i<n; i++){
+            arr[i]=array2[i];
+        }
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum=sum+array2[i];
+
+        }
+
+
+        for(int x:array2){
+            System.out.println(x);
         }
 
     }
@@ -26,5 +47,7 @@ public class MinSum {
         for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
         }
+        minSum(n,k,arr);
     }
 }
+
