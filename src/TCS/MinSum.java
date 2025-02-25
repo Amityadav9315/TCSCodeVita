a02d1df474e88a559eda5cfbdd52976f326cffd4
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-25 09:15:13 +0530

    fkgggdjhf

---
+++ src/TCS/MinSum.java
@@ -0,0 +1,25 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class MinSum {
+
+
+    static void minSum(int n,int r,int[] arr){
+
+        for(int i=0; i<n; i++){
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+}
