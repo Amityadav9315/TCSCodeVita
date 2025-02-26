13daac7a1aaa467107a37156dcf2059516c7376c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-26 23:25:09 +0530

    feqnw

---
+++ src/TCS/RailwayStation.java
@@ -0,0 +1,23 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class RailwayStation {
+
+    static void railwayStation(int n,int a,int b){
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++){
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+        }
+
+
+    }
+}
