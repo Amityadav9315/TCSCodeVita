d08db0250d45efe0de188cbf6a5ab11984f42af5
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-28 14:13:25 +0530

    fak.bfs

--- src/TCS/CriticalPlanets.java
+++ src/TCS/CriticalPlanets.java
@@ -8,9 +8,13 @@ public class CriticalPlanets {
     static void criticalPlanets(){
 
 
+
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+
 
     }
 }
