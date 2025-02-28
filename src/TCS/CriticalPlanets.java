47310dbc165803d0dda7726410d20a9d70d4ab2e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-02-28 14:05:29 +0530

    fdk.bsgkn

---
+++ src/TCS/CriticalPlanets.java
@@ -0,0 +1,16 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class CriticalPlanets {
+
+
+    static void criticalPlanets(){
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
