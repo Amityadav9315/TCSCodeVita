001a9b4584a3e6f67d00281f51af7aa27654ce90
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-01 21:43:56 +0530

    flhrgslhrs

---
+++ src/TCS/ZeroCount.java
@@ -0,0 +1,30 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class ZeroCount {
+        public static void main (String[] args) throws java.lang.Exception {
+            Scanner sc = new Scanner(System.in);
+            int l = sc.nextInt();
+            int k = sc.nextInt();
+
+            if (k == 0) {
+                System.out.println(l);
+            }
+
+            else if (k == l) {
+                System.out.println(0);
+            } else {
+                int num_gaps = k + 1;
+                int base_zeros = (l - k) / num_gaps;
+                int extra_zeros = (l - k) % num_gaps;
+                if (extra_zeros > 0) {
+                    System.out.println(base_zeros + 1);
+                } else {
+                    System.out.println(base_zeros);
+                }
+            }
+        }
+    }
+
+
