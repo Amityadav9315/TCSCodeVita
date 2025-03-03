c84415b9fa00d2a7b45b3ba8917b6065ab835984
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-03 13:33:27 +0530

    fewnklrw

--- src/TCS/perfectSquare.java
+++ src/TCS/perfectSquare.java
@@ -8,13 +8,27 @@ public class perfectSquare {
 
 
     static void PerfectSquare(int n){
-        for(int i=0;)
+        int count=0;
+        for(int i=1; i<n; i++){
+            if(n%i==i){
+                count=1;
+
+            }
+        }
+        if(count==1){
+            System.out.println("Perfect square");
+        }
+        else{
+            System.out.println("Not Prime Number");
+        }
 
 
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+
+        PerfectSquare(n);
 
     }
 }
