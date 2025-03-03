5d722195bf975f55fe03d8ed82e554c5b88109f0
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-03 13:06:32 +0530

    fadnmff

---
+++ src/TCS/SquareFreeNumber.java
@@ -0,0 +1,15 @@
+package TCS;
+
+import java.util.Scanner;
+
+public class SquareFreeNumber {
+
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+
+
+
+    }
+}
