f51388e9db205f5cca548245866478fbf114078c
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-03 13:09:41 +0530

    fsm,sghjdkfhg

--- src/TCS/SquareFreeNumber.java
+++ src/TCS/SquareFreeNumber.java
@@ -5,9 +5,27 @@ import java.util.Scanner;
 public class SquareFreeNumber {
 
 
+
+
+
+    static void squareFreeNumber(int n){
+        for(int i=0; i<n; i++){
+            if(n%i==0){
+
+            }
+        }
+    }
+
+
+    {
+
+
     public static void main(String[] args) {
 
         Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        }
 
 
 
