3b015a0f7871d8ac3d1b5b66277e7da86bf009e1
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-03 13:28:11 +0530

    ffghrty

--- src/TCS/perfectSquare.java
+++ src/TCS/perfectSquare.java
@@ -2,32 +2,19 @@ package TCS;
 
 import java.util.Scanner;
 
-public class SquareFreeNumber {
+public class perfectSquare {
 
 
 
 
+    static void PerfectSquare(int n){
+        for(int i=0;)
 
-    static void squareFreeNumber(int n){
-        for(int i=0; i<n; i++){
-            if(n%i==0){
 
-            }
-        }
     }
-
-
-
-
-
     public static void main(String[] args) {
-
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        }
-
-
-
     }
-
+}
