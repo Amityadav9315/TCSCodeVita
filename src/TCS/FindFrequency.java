8434b476d37d0b49189cb4d891a7c7a7022bc46e
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-26 21:21:47 +0530

    fdaghfadkj

--- src/TCS/FindFrequency.java
+++ src/TCS/FindFrequency.java
@@ -2,7 +2,7 @@ package TCS;
 
 import java.util.Scanner;
 
-public class FindFrequency {
+public class  FindFrequency {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
@@ -16,19 +16,16 @@ public class FindFrequency {
         for (int i = 0; i < n; i++) {
             numbers[i] = scanner.nextInt();
             if (numbers[i] > max) {
-                max = numbers[i]; // Find maximum number
+                max = numbers[i];
             }
         }
 
-        // Create frequency array
-        int[] count = new int[max + 1]; // Array to store frequency
+        int[] count = new int[max + 1];
 
-        // Count occurrences
         for (int num : numbers) {
             count[num]++;
         }
 
-        // Display results
         System.out.println("Number Frequencies:");
         for (int i = 0; i <= max; i++) {
             if (count[i] > 0) {
