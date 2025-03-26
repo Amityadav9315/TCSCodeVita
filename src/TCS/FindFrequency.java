7fb6062ef43b66763ce160e5921e11317ed69baa
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-26 21:35:25 +0530

    fdgjfj

--- src/TCS/FindFrequency.java
+++ src/TCS/FindFrequency.java
@@ -3,30 +3,39 @@ package TCS;
 import java.util.Scanner;
 
 public class FindFrequency {
-
-
-    static void findFrequency(int n,int[] arr){
-        int visited=-1;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-            if(arr[i]==arr[j]){
-                visited=1;
-            }
-
-
-        }
-
-
-
-
-    }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+        System.out.println("Enter the numbers:");
+
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+            if (numbers[i] > max) {
+                max = numbers[i]; // Find maximum number
+            }
         }
 
+        // Create frequency array
+        int[] count = new int[max + 1]; // Array to store frequency
+
+        // Count occurrences
+        for (int num : numbers) {
+            count[num]++;
+        }
+
+        // Display results
+        System.out.println("Number Frequencies:");
+        for (int i = 0; i <= max; i++) {
+            if (count[i] > 0) {
+                System.out.println(i + " -> " + count[i]);
+            }
+        }
+
+        scanner.close();
     }
 }
