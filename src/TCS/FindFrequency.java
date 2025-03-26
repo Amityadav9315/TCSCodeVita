299a236fe9fbc88e7d04ecf2829cef7bc806f1fb
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2025-03-26 21:18:40 +0530

    fwekjbfde

--- src/TCS/FindFrequency.java
+++ src/TCS/FindFrequency.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class FindFrequency {
 
 
-    static void findFrequency(){
+    static void findFrequency(int n,int[] arr){
+        int visited=-1;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+            if(arr[i]==arr[j]){
+                visited=1;
+            }
+        }
+
+
 
 
     }
-
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
